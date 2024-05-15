66232f77b1d4687c79a64105befec3240b9b0891
Hassan Ali <139656401+hasanbut17314@users.noreply.github.com>
2024-05-15 13:04:30 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import { Provider } from 'react-redux'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todo'
+import { store } from './app/Store'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Provider store={store}>
+      <h1 className='text-3xl text-white bg-gray-800 p-4'>Redux - Toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </Provider>
+  )
+}
+
+export default App
---
+++ src/features/todo/todoSlice.js
@@ -0,0 +1,33 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+    todos: [{id: 1, text: 'Example todo', completed: false}]
+}
+
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState,
+    reducers: {
+        addTodo: (state, action) => {
+            const todo = {
+                id: nanoid(),
+                text: action.payload,
+                completed: false
+            }
+            state.todos.push(todo)
+        },
+        updateTodo: (state, action) => {
+            state.todos = state.todos.map((todo) => todo.id === action.payload.id ? {...todo, text: action.payload.text} : todo)
+        },
+        deleteTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+        },
+        toggleCompleted: (state, action) => {
+            state.todos = state.todos.map((todo) => todo.id === action.payload ? {...todo, completed: !todo.completed} : todo);
+          }          
+    }
+})
+
+export const {addTodo, deleteTodo, updateTodo, toggleCompleted} = todoSlice.actions
+
+export default todoSlice.reducer
